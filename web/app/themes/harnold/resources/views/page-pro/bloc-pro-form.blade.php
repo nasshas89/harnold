b03c65bded5fb7e59d67867e8f9832731da03ff4
Nassim <nassim@nassim-desktop>
2020-08-17 22:21:32 +0200

    add begin form

--- web/app/themes/harnold/resources/views/page-pro/bloc-pro-form.blade.php
+++ web/app/themes/harnold/resources/views/page-pro/bloc-pro-form.blade.php
@@ -1,3 +1,11 @@
 <section class="form-pro">
-    {!!do_shortcode( '[contact-form-7 id="24" title="Contact form 1"]')!!}
+    <div class="bloc-form">
+        <div class="bloc-form__head">
+            <h2>Commencez à livrer avec Stuart</h2>
+            <p>Pour débuter votre inscription, rien de plus simple : remplissez ce formulaire !</p>
+        </div>
+          
+        {!!do_shortcode( '[contact-form-7 id="24" title="Contact form 1"]')!!}
+       
+    </div>    
 </section>    
