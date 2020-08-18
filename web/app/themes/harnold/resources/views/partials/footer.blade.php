1c576c9e2463f5a07084836b533535e88e9e3a67
Nassim <nassim@nassim-desktop>
2020-08-18 16:34:18 +0200

    ajout bloc footer

--- web/app/themes/harnold/resources/views/partials/footer.blade.php
+++ web/app/themes/harnold/resources/views/partials/footer.blade.php
@@ -1,3 +1,28 @@
 <footer class="footer">
+  <div class="footer__up wrapper">
+    <div class="footer__up__button">
+      {{-- logo+button commander --}}
+      <div class="logo-div-footer">
+        <img class="logo-footer" src="@asset('images/building.svg')" alt="" >
+      </div>
+      <a href="" class="cta-buy">Commander</a>
+    </div>
+    <div class="footer__up__us">
+      <span>A propos :</span>
+      <p>dfghj</p>
+    </div>
+    <div class="footer__up__link">
+      <span>Rester connecter :</span>
+{{-- Rester connecter (reseaux sociaux + newsletter) --}}
+      <p>dfghj</p>
+    </div>    
+  </div>
   
+  <div class="footer__down wrapper">
+    <div class="footer__down__content">
+      <div><a href=""> Mention légale</a></div>
+      <div><a href=""> © 2020 Harnold</a></div>
+      <div><a href=""> CGV</a></div>
+   </div>
+  </div> 
 </footer>
