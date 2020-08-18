b0265cea729acd9d71e706ba36d8bfaa00750ae8
Nassim <nassim@nassim-desktop>
2020-08-18 15:15:26 +0200

    form delivery

--- web/app/themes/harnold/resources/views/page-pro/bloc-pro-form.blade.php
+++ web/app/themes/harnold/resources/views/page-pro/bloc-pro-form.blade.php
@@ -1,11 +1,14 @@
 <section class="form-pro">
+    {{-- <img class="city-image" src="@asset('images/building.svg')" alt="" >
+    <img class="farm-image" src="@asset('images/farm.svg')" alt="" > --}}
+
     <div class="bloc-form">
         <div class="bloc-form__head">
             <h2>Commencez à livrer avec Stuart</h2>
             <p>Pour débuter votre inscription, rien de plus simple : remplissez ce formulaire !</p>
         </div>
-          
-        {!!do_shortcode( '[contact-form-7 id="24" title="Contact form 1"]')!!}
-       
+        <div class="short-code-form">
+            {!!do_shortcode( '[contact-form-7 id="24" title="Contact form 1"]')!!}
+        </div>
     </div>    
 </section>    
--- web/app/themes/harnold/resources/views/partials/footer.blade.php
+++ web/app/themes/harnold/resources/views/partials/footer.blade.php
@@ -1,5 +1,3 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
-  </div>
+<footer class="footer">
+  
 </footer>
