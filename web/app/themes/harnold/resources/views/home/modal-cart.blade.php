13087e6f2ff55664b77068f30f8af45f27f3d19c
Nassim <nassim@nassim-desktop>
2020-09-17 06:29:48 +0200

    add filtre all page sur modal panier

--- web/app/themes/harnold/resources/views/home/modal-cart.blade.php
+++ web/app/themes/harnold/resources/views/home/modal-cart.blade.php
@@ -1,16 +1,18 @@
-<div class="sidenav">
-    <div class="cart-modal-up">
-        <div class="picto-cart-up">
-            <img src="@asset('images/cart.svg')" alt="" >
-        </div>
-        <p>Votre panier</p>    
-        {{-- <a href="" class="js-closeModalCart" >&times;</a> --}}
-        <div class="cross-cart-up">
-            <img class="js-closeModalCart" src="@asset('images/cross.svg')" alt="" >
-        </div>
+
+    <div class="sidenav">
+        <div class="bloc-sidenav">
+            <div class="cart-modal-up">
+                <div class="picto-cart-up">
+                    <img src="@asset('images/cart.svg')" alt="" >
+                </div>
+                <p>Votre panier</p>    
+                {{-- <a href="" class="js-closeModalCart" >&times;</a> --}}
+                <div class="cross-cart-up">
+                    <img class="js-closeModalCart" src="@asset('images/cross.svg')" alt="" >
+                </div>
+            </div>
+            <div class="">
+                Frais de livraison de 2.5 euros pour le livreur.
+            </div>  
+        </div> 
     </div>
-    <a href="#">About</a>
-    <a href="#">Services</a>
-    <a href="#">Clients</a>
-    <a href="#">Contact</a>
-</div>
