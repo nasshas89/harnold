650a90683a351e631059c82c9e6f6ff0931bd8e4
Nassim <nassim@nassim-desktop>
2020-09-20 20:38:25 +0200

    add cart product test

--- web/app/themes/harnold/resources/views/home/product/card-product.blade.php
+++ web/app/themes/harnold/resources/views/home/product/card-product.blade.php
@@ -43,4 +43,49 @@
         </div>    
     </div>
 </div>
+
+<div class="tiesto">
+    <div class="product-card">
+    <div class="product-card__image">
+        <img  src="http://www.freefoodphotos.com/imagelibrary/herbs/slides/chilis.jpg" alt="fruits et légumes bio rennes">
+
+        <span class="rond-cart" href="#"><img src="@asset('images/cart-white.svg')" alt="cart picto"></span>
+    </div>
+    <div class="content-product-car">
+        <div class="content-product-car__title">
+            Oignon Jaune
+        </div>
+        <div class="content-product-car__origine">
+            <p>
+                Origine :
+                <span>
+                        Bretagne, ile et vilaine ..
+                </span>
+                <span class="texte-more">En savoit + 
+                    <img class="know-more" src="@asset('images/triangle-center.svg')" alt="cart picto">
+                </span>
+            </p>
+            <div class="content-product-car__info js-hidden-more">
+                <p>Information : Producteur d'ile et vilaine. Eri.
+                </p>
+            </div>
+        </div>
+        
+            
+        <div class="input-quantity">
+            <div class="content-product-car__price">
+                <p class="prix"><span>3,5€ </span></p>
+            </div>
+            <div class="num-block skin-2">
+                <div class="num-in">
+                    <span class="minus dis"></span>
+                    <input type="text" class="in-num" value="1" readonly="">
+                    <span class="plus"></span>
+                </div>
+                </div>
+
+            </div>     
+        </div>    
+    </div>
+</div>
         
