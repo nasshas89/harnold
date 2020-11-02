e6d647937e6de0690777143629ff92f41888ac96
Nassim <nassim@nassim-desktop>
2020-11-02 23:06:15 +0100

    add scroll on header

--- web/app/themes/harnold/resources/assets/scripts/components/header.js
+++ web/app/themes/harnold/resources/assets/scripts/components/header.js
@@ -1,10 +1,19 @@
 export default {
-    init() {
-      // JavaScript to be fired on the home page
-      console.log('tchooo')
-    },
-    finalize() {
-      // JavaScript to be fired on the home page, after the init JS
-    },
-  };
-  
+  init() {
+    // JavaScript to be fired on the home page
+    $(window).scroll(this.sticky);
+  },
+  sticky: function (event) {
+    event.preventDefault();
+    if (window.scrollY >= 30) {
+      $('.header-up').addClass('is-hidden');
+      $('.header-harnold').addClass('is-fixe');
+    } else if (window.scrollY <= 30) {
+      $('.header-up').removeClass('is-hidden');
+      $('.header-harnold').removeClass('is-fixe');
+    }
+  },
+  finalize() {
+
+  },
+};
