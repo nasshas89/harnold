29b324ff3389a90f79b6654cd7e4b80715f15c79
Nassim <nassim@nassim-desktop>
2020-11-03 20:52:09 +0100

    menu mob add

--- web/app/themes/harnold/resources/assets/scripts/components/modalMenuBurger.js
+++ web/app/themes/harnold/resources/assets/scripts/components/modalMenuBurger.js
@@ -3,6 +3,9 @@ export default {
         let openBurger= $('#js-open-burger');
         openBurger.on('click', this.burgerModal);
 
+        let closeButton= $('.close-button');
+        closeButton.on('click', this.closeburgerModal);
+
     },
         
     burgerModal: function (event) {
@@ -10,4 +13,12 @@ export default {
         $('.modal-menu-mobile').addClass('is-active');
         $('body').addClass('hidden-scroll');
       },
+
+      closeburgerModal: function (event) {
+        event.preventDefault();
+        $('.modal-menu-mobile').removeClass('is-active');
+        $('body').removeClass('hidden-scroll');
+      },
   };
+
+  
