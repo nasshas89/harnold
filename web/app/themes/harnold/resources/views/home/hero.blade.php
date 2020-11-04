1e02bc64fab0f2646bc912e49ac9f97936e396b5
Nassim <nassim@nassim-desktop>
2020-11-04 22:59:46 +0100

    hreo home done

--- web/app/themes/harnold/resources/views/home/hero.blade.php
+++ web/app/themes/harnold/resources/views/home/hero.blade.php
@@ -1,25 +1,13 @@
 <div class="hero-home" >
-    <div class="hero-home__left">
-        <img class="hero-image-home" src="@asset('images/livreur.PNG')" alt="picto menu">
-        <div class="hero-home-content">
-            <h1>fruits et légumes bio, locaux, livré à vélo </h1>
-            <img class="arrow-hero-home" src="@asset('images/right-arrow.svg')" alt="picto menu">
+    <div class="hero-home__circular">
+        <div class="hero-home__circular__content">
+            <h1 class="hero-home__circular__content__title"> Fruits et Légumes</h1>
+            <p class="title-under">Bio - Locaux livré à vélo !</p>
+            <p class="title-under__ligne">Service disponible uniquement à Rennes</p>
+            <p class="title-number">Tous les <span class="count" data-counter="300">0 </span> km nous plantons un arbre en zones urbaine</p>
         </div>
     </div>
-
     <div class="hero-home__right">
-        <div class="hero-home__right__card">
-            <div class="hero-home__right__card__title">
-                <p>Notre mission</p>
-            </div>
-            <div class="hero-home__right__card__content">
-                <p >" Planter un arbres en zone urbaine tout les " </p>
-            </div> 
-            <div class="hero-home__right__card__circular">
-                <p class="count" data-counter="300">0 </p>
-                <p>km parcouru</p>
-            </div>
-                 
-        </div>    
-    </div>        
+        <img class="hero-image-home" src="@asset('images/illustration-hero.svg')" alt="picto menu">
+    </div>
 </div>    
