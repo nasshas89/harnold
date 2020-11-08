ffb543b966e30022210e685a11396e57b0172821
Nassim <nassim@nassim-desktop>
2020-11-08 19:48:09 +0100

    custom banner promo desk et mob

--- web/app/themes/harnold/resources/views/components/sticky-promo.blade.php
+++ web/app/themes/harnold/resources/views/components/sticky-promo.blade.php
@@ -1,6 +1,24 @@
 <div class="sticky-promo">
-       <div class="wrapper">
-           <p>kkkkk</p>
+    <div class="wrapper sticky-promo__bloc">
+        <div class="sticky-promo__bloc__info">
+            <h3>Quiz de la semaine</h3>
+            <p>Recevez un code promo <span>-10%</span> csur votre commande cette semaine</p>
+            
         </div>
+        <div class="sticky-promo__bloc__question">
+            <h3>Quel fruit pousse dans un arbre ?</h3>
+            <div class="answer">
+                <form  method="POST">
+                    <label class="mail-box"><input type="text" name="mail" placeholder="Votre Email" required=""></label>
+
+                    <label class="answer-box"  for="rep1"> Fraise<input type="checkbox" name="rep1" placeholder="Banane" ></label>
+
+                    <label class="answer-box" for="rep2"> banane<input   type="checkbox"  name="rep2" placeholder="Fraise"></label>
+
+                    <input class="submit-box" type="submit" value="Valider">
+                  </form>
+            </div>
+        </div>   
+    </div>
 </div> 
 {{-- https://www.w3schools.com/cssref/tryit.asp?filename=trycss3_transition-property --}}
